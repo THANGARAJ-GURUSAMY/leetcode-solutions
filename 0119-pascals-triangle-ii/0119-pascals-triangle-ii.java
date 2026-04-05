30dad2a55b3cfb3a82010850e4a489e672666fe3
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-04-05 20:41:21 +0530

    Time: 0 ms (100%), Space: 42 MB (98.34%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row=new ArrayList<>();
+        long val=1;
+        for(int i=0;i<=rowIndex;i++)
+        {
+            row.add((int)val);
+            if(i<rowIndex)
+            {
+            val=val*(rowIndex-i)/(i+1);
+            }
+        }
+        return row;
+    }
+}
