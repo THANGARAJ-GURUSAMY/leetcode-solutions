590a9540e66f436a0558cbb27725bf1c124ade82
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-05-03 12:32:21 +0530

    Time: 2 ms (76.01%), Space: 43.9 MB (49.66%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray())
+        {
+            if(Character.isLetter(c))
+            {
+                stack.push(c);
+            }
+            else
+            {
+                if(!stack.isEmpty())
+                {
+                    stack.pop();
+                }
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
