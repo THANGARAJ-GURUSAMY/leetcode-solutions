0cbc23ad6ed45acd2d6ba34475ea7d3b6e246eb9
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-05-14 18:24:29 +0530

    Time: 6 ms (15.24%), Space: 45.4 MB (9.71%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,25 +1,22 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        List<Integer> l=new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
         Arrays.sort(nums);
-        int n=nums[nums.length-1];
-        for(int i=1;i<n;i++)
-        {
+        int n = nums[nums.length-1];
+        for(int i=1; i<n; i++){
             l.add(i);
         }
         l.add(n);
         l.add(n);
-        if(nums.length!=l.size())
-        {
+        if(l.size()!=nums.length){
             return false;
         }
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=l.get(i))
-            {
+        for(int i=0; i<n; i++){
+            if(nums[i]!=l.get(i)){
                 return false;
             }
         }
         return true;
+
     }
 }
