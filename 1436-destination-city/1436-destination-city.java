2534dd67fe86e6a39ee264573a72ae22c95ee0d1
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-06-16 11:23:34 +0530

    Time: 2 ms (100%), Space: 44.9 MB (80.02%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set=new HashSet<>();
+        for(List<String> p:paths)
+        {
+            set.add(p.get(0));
+        }
+        for(List<String> p:paths)
+        {
+            if(!set.contains(p.get(1)))
+            {
+                return p.get(1);
+            }
+        }
+        return "";
+    }
+}
