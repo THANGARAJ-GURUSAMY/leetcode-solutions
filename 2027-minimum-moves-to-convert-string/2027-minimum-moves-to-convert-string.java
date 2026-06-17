036bea976a7e96edfb9b2bf4f428de6ea70aa672
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-06-17 09:23:45 +0530

    Time: 1 ms (76.71%), Space: 42.8 MB (50.93%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int c=0;
+        int i=0;
+        while(i<s.length())
+        {
+            if(s.charAt(i)=='X')
+            {
+                c++;
+                i+=3;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return c;
+    }
+}
