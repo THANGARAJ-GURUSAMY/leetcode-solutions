812b8b2bac9d2117e651f2147626ec96f0b46c1d
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-06-17 22:26:44 +0530

    Time: 0 ms (100%), Space: 43.2 MB (89.39%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String greatestLetter(String s) {
+        for(char c='Z';c>='A';c--)
+        {
+            if(s.indexOf(c)!=-1&&s.indexOf(Character.toLowerCase(c))!=-1)
+            {
+                return String.valueOf(c);
+            }
+        }
+        return "";
+    }
+}
