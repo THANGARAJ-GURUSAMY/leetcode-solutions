f68e28f1ae2dca04fa8cd517dff817d7a9247976
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-06-17 16:50:09 +0530

    Time: 1 ms (89.69%), Space: 43.3 MB (18.83%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int convertTime(String current, String correct) {
+        int cur=Integer.parseInt(current.substring(0,2))*60+Integer.parseInt(current.substring(3));
+        int cor=Integer.parseInt(correct.substring(0,2))*60+Integer.parseInt(correct.substring(3));
+        int d=cor-cur;
+        int count=0;
+        int[] op={60,15,5,1};
+        for(int t:op)
+        {
+            count+=d/t;
+            d%=t;
+        }
+        return count;
+    }
+}
