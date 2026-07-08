692432f79a8e4f149f47e6a9e259a640490b43fe
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-07-08 07:56:33 +0530

    Time: 89 ms (6.67%), Space: 151.8 MB (6.67%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int[] sumAndMultiply(String s, int[][] queries) {
+
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> digits = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int d = s.charAt(i) - '0';
+            if (d != 0) {
+                pos.add(i);
+                digits.add(d);
+            }
+        }
+
+        int m = digits.size();
+
+        long[] prefixSum = new long[m + 1];
+        long[] prefixValue = new long[m + 1];
+        long[] pow10 = new long[m + 1];
+
+        pow10[0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            prefixSum[i + 1] = prefixSum[i] + digits.get(i);
+            prefixValue[i + 1] = (prefixValue[i] * 10 + digits.get(i)) % MOD;
+            pow10[i + 1] = (pow10[i] * 10) % MOD;
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            int left = lowerBound(pos, l);
+            int right = upperBound(pos, r) - 1;
+
+            if (left > right) {
+                ans[i] = 0;
+                continue;
+            }
+
+            int len = right - left + 1;
+
+            long sum = prefixSum[right + 1] - prefixSum[left];
+
+            long x = (prefixValue[right + 1]
+                    - prefixValue[left] * pow10[len] % MOD + MOD) % MOD;
+
+            ans[i] = (int) (x * sum % MOD);
+        }
+
+        return ans;
+    }
+
+    private int lowerBound(ArrayList<Integer> list, int target) {
+        int l = 0, r = list.size();
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (list.get(mid) >= target)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+
+    private int upperBound(ArrayList<Integer> list, int target) {
+        int l = 0, r = list.size();
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (list.get(mid) > target)
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
