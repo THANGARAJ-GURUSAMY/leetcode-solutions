9fb287f7b7e948b8b4d6675c7bb1fa9af1377bb3
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-07-10 07:09:36 +0530

    Time: 83 ms (91.67%), Space: 270.6 MB (87.5%) - LeetHub

---
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.java
@@ -0,0 +1,88 @@
+class Solution {
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+          int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int[] pos = new int[n];
+        for (int i = 0; i < n; i++) {
+            pos[arr[i][1]] = i;
+        }
+
+  
+        int[] comp = new int[n];
+        int cid = 0;
+        comp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i][0] - arr[i - 1][0] > maxDiff)
+                cid++;
+            comp[i] = cid;
+        }
+
+       
+        int[] right = new int[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j + 1 < n &&
+                   arr[j + 1][0] - arr[i][0] <= maxDiff)
+                j++;
+            right[i] = j;
+            if (j < i + 1) j = i + 1;
+        }
+
+        int LOG = 18;
+
+        int[][] jump = new int[LOG][n];
+
+        for (int i = 0; i < n; i++)
+            jump[0][i] = right[i];
+
+        for (int k = 1; k < LOG; k++) {
+            for (int i = 0; i < n; i++) {
+                jump[k][i] = jump[k - 1][jump[k - 1][i]];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int qi = 0; qi < queries.length; qi++) {
+
+            int u = pos[queries[qi][0]];
+            int v = pos[queries[qi][1]];
+
+            if (u == v) {
+                ans[qi] = 0;
+                continue;
+            }
+
+            if (u > v) {
+                int t = u;
+                u = v;
+                v = t;
+            }
+
+            if (comp[u] != comp[v]) {
+                ans[qi] = -1;
+                continue;
+            }
+
+            int cur = u;
+            int steps = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (jump[k][cur] < v) {
+                    cur = jump[k][cur];
+                    steps += 1 << k;
+                }
+            }
+
+            ans[qi] = steps + 1;
+        }
+
+        return ans;
+    }
+}
