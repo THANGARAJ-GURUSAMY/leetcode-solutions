9018a7e5991566f147676787dfae68dd446b3d48
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-08-05 18:58:09 +0530

    Time: 2 ms (46.02%), Space: 46.6 MB (89.44%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -1,25 +1,20 @@
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
         int n=nums.length;
-        int max=0;
-        for(int i=0;i<n;i++)
+        int ans=1,count=1;
+        for(int i=1;i<nums.length;i++)
         {
-            int c=1;
-            int num=nums[i];
-            for(int j=i+1;j<n;j++)
+            if(nums[i-1]<nums[i])
             {
-                if(nums[j]>num)
-                {
-                    c++;
-                    num=nums[j];
-                }
-                else
-                {
-                    break;
-                }
+                count++;
             }
-            max=Math.max(max,c);
+            else
+            {
+                count=1;
+            }
+            ans=Math.max(ans,count);
+            
         }
-        return max;
+        return ans;
     }
 }
