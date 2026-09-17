f8a0432f5f96bafffe446086ae812d41ce4765ae
THANGARAJ-GURUSAMY <thangarajm2006@gmail.com>
2026-09-17 11:30:28 +0530

    Time: 0 ms (100%), Space: 42.7 MB (70.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,53 +14,19 @@
  * }
  */
 class Solution {
-    List<Integer> l1 = new ArrayList<>();
-    List<Integer> l2 = new ArrayList<>();
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        preorder(p);
-        preorder1(q);
-        if(l1.size()!=l2.size())
+        if(p==null&&q==null)
+        {
+            return true;
+        }
+        if(p==null||q==null)
         {
             return false;
         }
-        for(int i=0;i<l1.size();i++)
+        if(p.val!=q.val)
         {
-            if(l1.get(i)==null&&l2.get(i)==null)
-            {
-
-                continue;
-            }
-            if(l1.get(i)==null||l2.get(i)==null)
-            {
-                return false;
-            }
-            if(!l1.get(i).equals(l2.get(i)))
-            {
-                return false;
-            }
+            return false;
         }
-        return true;
-    }
-    public List<Integer> preorder(TreeNode root)
-    {
-        if(root == null){
-            l1.add(null);
-            return l1;
-        }
-        l1.add(root.val);
-        preorder(root.left);
-        preorder(root.right);
-        return l1;
-    }
-    public List<Integer> preorder1(TreeNode root)
-    {
-        if(root == null){
-            l2.add(null);
-            return l2;
-        }
-        l2.add(root.val);
-        preorder1(root.left);
-        preorder1(root.right);
-        return l2;
-    }
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    } 
 }
